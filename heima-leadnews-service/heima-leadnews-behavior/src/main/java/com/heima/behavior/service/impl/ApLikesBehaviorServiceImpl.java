d5101965c95a71a61248d10d501cf0636d2dcba6
xuluming <2761151386@qq.com>
2024-06-04 11:37:13 +0800

    kafkaStream实时计算热点数据

--- heima-leadnews-service/heima-leadnews-behavior/src/main/java/com/heima/behavior/service/impl/ApLikesBehaviorServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-behavior/src/main/java/com/heima/behavior/service/impl/ApLikesBehaviorServiceImpl.java
@@ -3,13 +3,16 @@ package com.heima.behavior.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.heima.behavior.service.ApLikesBehaviorService;
 import com.heima.common.constants.BehaviorConstants;
+import com.heima.common.constants.HotArticleConstants;
 import com.heima.common.redis.CacheService;
 import com.heima.model.behavior.dtos.LikesBehaviorDto;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.mess.UpdateArticleMess;
 import com.heima.model.user.pojos.ApUser;
 import com.heima.utils.common.AppThreadLocalUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.data.Json;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -24,6 +27,9 @@ public class ApLikesBehaviorServiceImpl implements ApLikesBehaviorService {
     @Autowired
     private CacheService cacheService;
 
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
     @Override
     public ResponseResult like(LikesBehaviorDto dto) {
 
@@ -37,6 +43,9 @@ public class ApLikesBehaviorServiceImpl implements ApLikesBehaviorService {
         if (user == null) {
             return ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
         }
+        UpdateArticleMess mess = new UpdateArticleMess();
+        mess.setArticleId(dto.getArticleId());
+        mess.setType(UpdateArticleMess.UpdateArticleType.LIKES);
 
         //3.点赞  保存数据
         if (dto.getOperation() == 0) {
@@ -47,12 +56,14 @@ public class ApLikesBehaviorServiceImpl implements ApLikesBehaviorService {
             // 保存当前key
             log.info("保存当前key:{} ,{}, {}", dto.getArticleId(), user.getId(), dto);
             cacheService.hPut(BehaviorConstants.LIKE_BEHAVIOR + dto.getArticleId().toString(), user.getId().toString(), JSON.toJSONString(dto));
+            mess.setAdd(1);
         } else {
             // 删除当前key
             log.info("删除当前key:{}, {}", dto.getArticleId(), user.getId());
             cacheService.hDelete(BehaviorConstants.LIKE_BEHAVIOR + dto.getArticleId().toString(), user.getId().toString());
+            mess.setAdd(-1);
         }
-
+        kafkaTemplate.send(HotArticleConstants.HOT_ARTICLE_SCORE_TOPIC, JSON.toJSONString(mess));
 
         return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
 
